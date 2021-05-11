544398b945de6e71603fc7ddf72c9d1a6659b462
lucasmercado101 <lucasmercado101@gmail.com>
2021-05-11 16:40:12 -0300

    fixed getAllProducts route in api

--- src/api.js
+++ src/api.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const axiosInst = axios.create({
-  baseURL: "http://localhost:3001",
+  baseURL: "http://localhost:7000",
   withCredentials: true
 });
 // axios.defaults.baseUrl = "http://localhost:6000";
 
 export function getAllProducts() {
-  return axiosInst.get("/products").then((res) => res.data);
+  return axiosInst.get("/business/products").then((res) => res.data);
 }
 
 export function getProductById(id) {
