ac30b4218e1ff6ea4771b58a5500b5d00d5adce0
billybautista <soybilder@gmail.com>
2021-05-27 15:51:50 -0300

    Change color

--- src/Theme.jsx
+++ src/Theme.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider, createMuiTheme } from "@material-ui/core";
 
 function Theme({ children }) {
   const theme = createMuiTheme({
-    palette: { primary: { main: "#E49012" }, background: { default: "#fff" } }
+    palette: { primary: { main: "#ff8000" }, background: { default: "#fff" } },
   });
   // const theme = createMuiTheme({ palette: { type: "dark" } });
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
